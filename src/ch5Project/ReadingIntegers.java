b032627d8fb1ad47abf53db351a8b33765b29379
TaressaHalpin <thalpin2583@kvcc.edu>
2018-01-20 11:21:17 -0500

    READING INTEGERS

--- src/ch5Project/ReadingIntegers.java
+++ src/ch5Project/ReadingIntegers.java
@@ -1,33 +1,34 @@
-/*CHAPTER 5 PROJECT:
+/*
   
   READS INTEGERS
   
-  TARESSA HALPIN 
-  10-23-17*/
- 
-package ch5Project;
-
-import java.util.Scanner;
+ import java.util.Scanner;
 
 public class ReadingIntegers 
 
   {
 
 	//MAIN METHOD 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+
+  {
 		//SCANNER DECLARATION 
 		Scanner input = new Scanner(System.in);
+		
 		//PROMPTS USER FOR INPUT 
 		System.out.println("Please enter the numbers: ");
+		
 		//GETS VALUES FOR LINE INPUT 
 		String values = input.nextLine();
+		
 		//CREATES MAX INTEGER
 		int max = 0;
+		
 		//CREATES COUNT INTEGER 
 		int count = 0; 
 		
 		
-		//INTEGER FOR LOOP
+	//FOR LOOP
         for (int i = 0; i < values.length(); i++)
         {
         	//CREATES CHARACTER C
@@ -50,15 +51,14 @@ public class ReadingIntegers
         			max = value; 
         			
         		}
+
         		//ELSE IF STATEMENT: VALUE EQUAL TO MAX
         		else if (value == max)
         		{
         			//INCREMENT COUNT 
-        			count++; 
-        			       			
+        			count++;       			
         		}
-        		
-        		
+        		        		
         		//CHECK CURRENT DIGIT TO SEE IF IT IS GREATER THAN MAX, 
         		//IF IT IS - SET NEW MAX VALUE TO THAT CHARACTER - SET COUNT TO 1. 
         		//CHECK TO SEE IF CURRENT DIGIT IS EQUAL TO MAX (ELSE) THEN INCREMENT COUNT BY 1. 
@@ -66,18 +66,12 @@ public class ReadingIntegers
         	}
 
          }
-		        //PRINT STATEMENT: FINDS LARGEST NUMBER 
+		 //PRINT STATEMENT: FINDS LARGEST NUMBER 
                 System.out.println("The Largest Number of List: " + max);
                 
                 //PRINT STATEMENT: LARGEST NUMBER OCCURANCE 
                 System.out.println("The Occurance of List Count: " + count);
-        
-               
-		
 		}
-		  		 
-		
-		
      } 
 
    
