376780114ce2bd1b6ae9a17cbee87cdeec9eef08
TaressaHalpin <thalpin2583@kvcc.edu>
2018-01-20 11:11:10 -0500

    READING INTEGERS

---
+++ src/ch5Project/ReadingIntegers.java
@@ -0,0 +1,84 @@
+/*CHAPTER 5 PROJECT:
+  
+  READS INTEGERS
+  
+  TARESSA HALPIN 
+  10-23-17*/
+ 
+package ch5Project;
+
+import java.util.Scanner;
+
+public class ReadingIntegers 
+
+  {
+
+	//MAIN METHOD 
+	public static void main(String[] args) {
+		//SCANNER DECLARATION 
+		Scanner input = new Scanner(System.in);
+		//PROMPTS USER FOR INPUT 
+		System.out.println("Please enter the numbers: ");
+		//GETS VALUES FOR LINE INPUT 
+		String values = input.nextLine();
+		//CREATES MAX INTEGER
+		int max = 0;
+		//CREATES COUNT INTEGER 
+		int count = 0; 
+		
+		
+		//INTEGER FOR LOOP
+        for (int i = 0; i < values.length(); i++)
+        {
+        	//CREATES CHARACTER C
+        	char c = values.charAt(i);
+        	//SETS CHARACTER EQUAL TO USER INPUT 
+        	if (c == ' ')  continue; 
+        	
+        	//IF STATEMENT TO MAKE CHARACTER A DIGIT 
+        	if (Character.isDigit(c))
+        	{
+        		      		
+        		//INT VALUE TO MAKE 'C' A NUMBER VALUE 
+        		int value = Character.getNumericValue(c); 
+        		//IF STATEMENT: VALUE GREATER THAN MAX
+        		if (value > max)
+        		{
+        			//SET COUNT TO 1
+        			count = 1; 
+        			//SET MAX EQUAL TO VALUE 
+        			max = value; 
+        			
+        		}
+        		//ELSE IF STATEMENT: VALUE EQUAL TO MAX
+        		else if (value == max)
+        		{
+        			//INCREMENT COUNT 
+        			count++; 
+        			       			
+        		}
+        		
+        		
+        		//CHECK CURRENT DIGIT TO SEE IF IT IS GREATER THAN MAX, 
+        		//IF IT IS - SET NEW MAX VALUE TO THAT CHARACTER - SET COUNT TO 1. 
+        		//CHECK TO SEE IF CURRENT DIGIT IS EQUAL TO MAX (ELSE) THEN INCREMENT COUNT BY 1. 
+        		//OUTPUT COUNT AND MAX AFTER LOOP 
+        	}
+
+         }
+		        //PRINT STATEMENT: FINDS LARGEST NUMBER 
+                System.out.println("The Largest Number of List: " + max);
+                
+                //PRINT STATEMENT: LARGEST NUMBER OCCURANCE 
+                System.out.println("The Occurance of List Count: " + count);
+        
+               
+		
+		}
+		  		 
+		
+		
+     } 
+
+   
+
